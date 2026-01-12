c443bc2cb908fe262fcf951e85db86b72166ac84
michanselm <michanselm@gmail.com>
2026-01-12 04:39:46 +0100

    N

--- app.js
+++ app.js
@@ -4,17 +4,15 @@ let schoolName = prompt("Enter your school name:");
 if (!schoolName) {
   schoolName = "Our School";
 }
-alert("Welcome to " + schoolName + "!");
-function mainMenu() {}
+function showMenu() {}
 const option = Number(
   prompt(
-    "Please choose an operation:\n1.  Register Student \n 2. View All Students\n 3. Search Student \n 4. Exit"
+    "Please choose an operation: \n1.Register Student \n 2. View All Students\n 3. Search Student \n 4. Exit"
   )
 );
-
+ let ppl = [];
 switch (option) {
-  case 1:
-    let ppl = [];
+  case 1: 
     let name = prompt("Enter student name:");
     if (!name) {
       alert("Student name cannot be empty");
@@ -26,44 +24,46 @@ switch (option) {
       break;
     }
 
-     let clas = prompt("Enter clas:");
+    let clas = prompt("Enter class:");
     if (!clas) {
       alert("clas cannot be empty");
       break;
     }
 
-     let score = prompt("Enter score:");
-    if (score < 1 ) {
+    let score = prompt("Enter score (1- 100):");
+    if (score < 1) {
       alert("Invalid Score");
       break;
-    } 
-    if (score > 100) { 
-      alert("Invalid Score")
+    }
+    if (score > 100) {
+      alert("Invalid Score");
       break;
     }
-    
-    let ans = prompt("Put in your grade");
 
-    if (isNaN(ans)) {
+    // let ans = prompt("Put in your grade");
+
+    if (isNaN(score)) {
       console.log("You did not put in a number");
-    } else if (ans > 100) {
+    } else if (score > 100) {
       console.log("Your score is not within the range");
-    } else if (ans > 69) {
-      console.log("You got an A");
-    } else if (ans > 59) {
-      console.log("You got a B");
-    } else if (ans > 49) {
-      console.log("You got a C");
-    } else if (ans > 39) {
-      console.log("You got a D");
-    } else if (ans > 31) {
-      console.log("You got an E");
-    } else if (ans < 29) {
-      console.log("You got an F");
+    } else if (score > 69) {
+      console.log("You got an A You Passed");
+    } else if (score > 59) {
+      console.log("You got a B  You Passed");
+    } else if (score > 49) {
+      console.log("You got a C You Passed");
+    } else if (score < 49) {
+      console.log("You got an D You failed");
+    } else if (score < 40) {
+      console.log("You got an F You Failed");
     }
-
-    ppl.push( name, age , clas, score );
+    
+    ppl.push(name, age, clas, score);
     console.log("student:", ppl);
     break;
-      
+     
+  function showMenu() {}
+  case 2:
+    
+    
 }
